b5d1659861394ec623db092370bd64981afa827c
Amir Mansour <swasat2009@hotmail.com>
2023-10-31 20:52:00 +0100

    coins removed

--- logic.js
+++ logic.js
@@ -32,25 +32,6 @@ class Player {
     this.thePlayer.style.bottom = this.positionY + "vh";
     this.coin();
   }
-  coin() {
-    if (this.coins.length === 0);
-
-    const coinElements = document.getElementsByClassName("coins");
-    for (let i = coinElements.length - 1; i >= 0; i--) {
-      const coin = this.coins[i];
-      const coinElement = coinElements[i];
-
-      if (
-        this.positionX < coinElement.positionX + coinElement.width &&
-        this.positionX + this.width > coinElement.positionX &&
-        this.positionY < coinElement.positionY + coinElement.height &&
-        this.positionY + this.height > coinElement.positionY
-      ) {
-      
-        
-      }
-    }
-  }
 }
 
 const jimmy = new Player();
@@ -79,11 +60,9 @@ class Money {
     this.height = 4;
     this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
     this.positionY = Math.floor(Math.random() * (100 - this.height + 1));
-    
+
     this.createDomElement();
   }
-
-  
 
   createDomElement() {
     this.theMoney = document.createElement("div");
@@ -103,7 +82,27 @@ class Money {
 function createCoin() {
   const coin = new Money();
   allTheCoins.push(coin);
-  setTimeout(createCoin, 1000);
 }
 
-createCoin();
+function checkCollison() {
+  for (let i = allTheCoins.length - 1; i >= 0; i--) {
+    const coin = allTheCoins[i];
+
+    if (
+      jimmy.positionX < coin.positionX + coin.width &&
+      jimmy.positionX + jimmy.width > coin.positionX &&
+      jimmy.positionY < coin.positionY + coin.height &&
+      jimmy.positionY + jimmy.height > coin.positionY
+    ) {
+      console.log(coin);
+      coin.theMoney.remove();
+      allTheCoins.splice(i, 1);
+    }
+  }
+}
+
+const myGameInterval = setInterval(() => {
+  checkCollison();
+}, 100);
+
+const coinInterval = setInterval(createCoin, 1000);
